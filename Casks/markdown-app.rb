5fb65fef8b1067de93285f94535cfb04fab9ffcf
Devin Schumacher <45643901+devinschumacher@users.noreply.github.com>
2023-10-11 04:03:29 -0700

    Update markdown-app.rb

--- Casks/markdown-app.rb
+++ Casks/markdown-app.rb
@@ -2,7 +2,7 @@ cask "markdown-app" do
   version "1.0.0"
   sha256 "8a51c03f1ff77c2b8e76da512070c23c5e69813d5c61732b3025199e5f0c14d5"
 
-  url "https://github.com/devinschumacher/markdown-app/releases/download/v#01/Markdown-App-#01.dmg",
+  url "https://github.com/devinschumacher/markdown-app/releases/download/v#{version}/Markdown-App-#{version}.dmg",
       verified: "github.com/devinschumacher/markdown-app/"
   name "Markdown App"
   desc "An application for editing and viewing Markdown files"
